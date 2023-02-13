be7d1b5cce6c41c97788e3b3fdf4902d8626c350
Josh Nichols <jnichols20091@gmail.com>
2023-02-12 23:35:04 -0600

    added a form for example sake

--- joshsite/models.py
+++ joshsite/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class MyModel(models.Model):
+    fullname = models.CharField(max_length=500)
+    mobile_number = models.IntegerField()
--- joshsite/views.py
+++ joshsite/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import MyModel
+from .forms import MyForm
 
 # Create your views here.
 
 
 def home(request):
-    return render(request,"index-app.html")
-
+    if request.method == "POST":
+        form = MyForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = MyForm()
+    return render(request, "index-app.html", {"form": form})
 
 def login(request):
     return render(request, "login.html")
@@ -19,3 +26,5 @@ def services(request):
 def contacts(request):
     return render(request, 'contacts.html')
 
+    
+    return render(request,"index-app.html")
